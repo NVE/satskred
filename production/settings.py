159fad5bc8a9a891c066329ed63e93df0e9f8b16
kmunve <kmu@nve.no>
2020-11-23 22:09:57 +0100

    created track list

--- production/settings.py
+++ production/settings.py
@@ -1,12 +1,27 @@
 import os
+from glob import glob
 from pathlib import Path
 from dotenv import load_dotenv
 load_dotenv()
 
 # for other options see https://github.com/theskumar/python-dotenv
 
-TARGET_DIR = Path(os.getenv("TARGET_DIR"))
+LOG_DIR = Path(os.getenv("LOG_DIR"))
+AREA_DIR = Path(os.getenv("AREA_DIR"))
 
 
 if __name__ == "__main__":
-    print(TARGET_DIR.absolute())
+    region = 'region_1016'
+    print(LOG_DIR.absolute())
+    print(AREA_DIR.absolute())
+    REGION_DIR = AREA_DIR / region
+    RADARMASK_DIR = REGION_DIR / 'preprocessed' / 'radar_masks'
+    print(RADARMASK_DIR.absolute())
+    os.chdir(RADARMASK_DIR.absolute())
+    track_list = glob('*.tif')
+    tracks = []
+    for track in track_list:
+        tracks.append(int(track.split('.')[0].split('_')[2]))
+
+    print(tracks)
+
