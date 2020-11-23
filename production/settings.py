234eff9dbce268d5df4a959fefa8b44928242fd5
kmunve <kmu@nve.no>
2020-11-23 22:36:51 +0100

    test in settings.py creates full run-command

--- production/settings.py
+++ production/settings.py
@@ -1,4 +1,5 @@
 import os
+from datetime import date, timedelta
 from glob import glob
 from pathlib import Path
 from dotenv import load_dotenv
@@ -6,22 +7,33 @@ load_dotenv()
 
 # for other options see https://github.com/theskumar/python-dotenv
 
-LOG_DIR = Path(os.getenv("LOG_DIR"))
-AREA_DIR = Path(os.getenv("AREA_DIR"))
+LOG_DIR = Path(os.getenv("LOG_DIR")).absolute()
+AREA_DIR = Path(os.getenv("AREA_DIR")).absolute()
 
 
 if __name__ == "__main__":
     region = 'region_1016'
-    print(LOG_DIR.absolute())
-    print(AREA_DIR.absolute())
+    LOG_FILE = LOG_DIR / "{0}.log".format(region)
     REGION_DIR = AREA_DIR / region
     RADARMASK_DIR = REGION_DIR / 'preprocessed' / 'radar_masks'
-    print(RADARMASK_DIR.absolute())
-    os.chdir(RADARMASK_DIR.absolute())
-    track_list = glob('*.tif')
-    tracks = []
-    for track in track_list:
-        tracks.append(int(track.split('.')[0].split('_')[2]))
 
-    print(tracks)
+    _now = date.today()
+    _offset = timedelta(days=13)
+    START_TIME = (_now - _offset).strftime('%Y-%m-%d')
+    STOP_TIME = _now.strftime('%Y-%m-%d')
+
+    os.chdir(RADARMASK_DIR)
+    track_list = glob('*.tif')
+    TRACKS = []
+    for track in track_list:
+        TRACKS.append(int(track.split('.')[0].split('_')[2]))
+
+    TRACKS = str(TRACKS).replace(',', '').replace('[', '').replace(']', '')
+
+    CMD = "satskred run {REGION_DIR} --starttime {START_TIME} --stoptime {STOP_TIME} --logfile {LOG_FILE} --tracks {TRACKS}".format(REGION_DIR=REGION_DIR, START_TIME=START_TIME, STOP_TIME=STOP_TIME, LOG_FILE=LOG_FILE, TRACKS=TRACKS)
+    print(LOG_DIR)
+    print(REGION_DIR)
+    print(RADARMASK_DIR)
+    print(TRACKS)
+    print(CMD)
 
