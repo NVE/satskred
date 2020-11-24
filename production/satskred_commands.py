a8641f5c37e7c1d88f2cdfbc6285c71c08a40c76
kmunve <kmu@nve.no>
2020-11-24 08:42:21 +0100

    Clean up

--- production/satskred_commands.py
+++
@@ -1,3 +0,0 @@
-
-
-"satskred run U:\SatSkred\sourcedata\areas\region_1016 --starttime 2020-11-01 --stoptime 2020-11-13 --logfile U:\SatSkred\log\region_1016.log --tracks [8, 15, 37, 44, 110, 117, 139, 146]"
